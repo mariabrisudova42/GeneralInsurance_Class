d2bbe8da4507c0e639585d9a9050f2271e57d22f
mariabrisudova42 <38209861+mariabrisudova42@users.noreply.github.com>
2018-04-15 11:04:35 +0200

    Update server.R

--- Lessons/Lesson1/Homework/server.R
+++ Lessons/Lesson1/Homework/server.R
@@ -1,4 +1,4 @@
-server<-function(input, output) {
+function(input, output) {
     output$plot <- renderPlot(
         ggplot(data = dt_KPI,
        mapping = aes_string(x = "Premium", y = "Expenses", colour = input$select)
